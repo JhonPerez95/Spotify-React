5e61735bd01d6bf7dd2be3bb5a911ba8abe158b7
jhonperez95 <jhonperez.j95@gmail.com>
2020-06-16 23:12:03 -0500

    token_access

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './style/App.css';
 
 function App() {
+  const params = getHashParams();
+
+  console.log(params);
+
+  function getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <a href="http://localhost:8888/">
+          <button>Login with Spotify</button>
+        </a>
+      </header>
     </div>
   );
 }
